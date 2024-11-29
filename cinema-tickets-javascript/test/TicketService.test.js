0c5ec49a18d4827eee2fb6b8eaa079fc319962f0
Ryan <ryanshaunsmith@gmail.com>
2024-11-29 09:58:36 +0000

    add tests for zero tickets and more than 20 tickets

--- cinema-tickets-javascript/test/TicketService.test.js
+++ cinema-tickets-javascript/test/TicketService.test.js
@@ -1,16 +1,37 @@
+import TicketTypeRequest from "../src/pairtest/lib/TicketTypeRequest";
 import TicketService from "../src/pairtest/TicketService";
 
 describe("AccountID validation", () => {
+  const newTickets = new TicketService();
   test("An error is thrown if AccountID is not a number", async () => {
-    const newTickets = new TicketService();
     expect(() => newTickets.purchaseTickets("ABCDDEEEE")).toThrowError(
       "Your account ID must be an integer"
     );
   });
   test("AccountID is a number greater than 0", async () => {
-    const newTickets = new TicketService();
+    // const newTickets = new TicketService();
     expect(() => newTickets.purchaseTickets(0)).toThrowError(
       "Your account ID must be a number greater than 0"
     );
+  });
+});
+
+describe("Valid tickets have been requested", () => {
+  test("An error is thrown if no tickets are requested", async () => {
+    const newTickets = new TicketService();
+    expect(() => newTickets.purchaseTickets(100)).toThrowError(
+      "No tickets requested"
+    );
+  });
+  test("An error is thrown if more than 20 tickets are requested", async () => {
+    let ticketTypeRequests = [];
+    for (let i = 1; i <= 21; i++) {
+      const ticket = new TicketTypeRequest("ADULT, 1");
+      ticketTypeRequests.push(ticket);
+    }
+    const newTickets = new TicketService();
+    expect(() =>
+      newTickets.purchaseTickets(100, ticketTypeRequests)
+    ).toThrowError("The maximum number of tickets you can purchase is 20");
   });
 });
